1fdef6ed3974c65d54ce59264cbc4f2439e3b126
Crowdbotics <team@crowdbotics.com>
2020-04-30 09:10:04 +0000

    Update models

--- backend/level_uhp_16439/wsgi.py
+++ backend/level_uhp_16439/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'level_uhp_16439.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "level_uhp_16439.settings")
 
 application = get_wsgi_application()
